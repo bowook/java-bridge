3aa4cad44630162e93ced820bd6b9bb9bbc81be4
bowook <mbosx316@naver.com>
2024-11-30 18:39:24 +0900

    feat : OutputView 기능 추가

--- src/main/java/bridge/view/OutputView.java
+++ src/main/java/bridge/view/OutputView.java
@@ -1,9 +1,21 @@
-package bridge;
+package bridge.view;
 
 /**
  * 사용자에게 게임 진행 상황과 결과를 출력하는 역할을 한다.
  */
 public class OutputView {
+    private final String START_MESSAGE = "다리 건너기 게임을 시작합니다.";
+
+    public void printStart() {
+        printMessage(START_MESSAGE);
+        printMessage(newLine());
+        printMessage(newLine());
+    }
+
+    public void printErrorMessage(String errorMessage) {
+        printMessage(errorMessage);
+        printMessage(newLine());
+    }
 
     /**
      * 현재까지 이동한 다리의 상태를 정해진 형식에 맞춰 출력한다.
@@ -19,5 +31,13 @@ public class OutputView {
      * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
     public void printResult() {
+    }
+
+    private void printMessage(String message) {
+        System.out.print(message);
+    }
+
+    private String newLine() {
+        return System.lineSeparator();
     }
 }
