f7aa69e8d918b3b1add0703af8560317e98a814d
bowook <mbosx316@naver.com>
2024-11-30 18:37:42 +0900

    feat : MoveCommand 기능 추가

--- src/main/java/bridge/domain/constant/MoveCommand.java
+++ src/main/java/bridge/domain/constant/MoveCommand.java
@@ -1,2 +1,26 @@
-package bridge.domain.constant;public enum MoveCommand {
+package bridge.domain.constant;
+
+import bridge.exception.CustomException;
+import bridge.exception.ErrorMessage;
+import java.util.Arrays;
+
+public enum MoveCommand {
+    UP("U"),
+    DOWN("D");
+    private final String moveCommand;
+
+    MoveCommand(final String moveCommand) {
+        this.moveCommand = moveCommand;
+    }
+
+    public static MoveCommand from(String input) {
+        return Arrays.stream(MoveCommand.values())
+                .filter(command -> command.moveCommand.equals(input))
+                .findAny()
+                .orElseThrow(() -> CustomException.from(ErrorMessage.MOVE_COMMAND_ERROR));
+    }
+
+    public String getMoveCommand() {
+        return moveCommand;
+    }
 }
