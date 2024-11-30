a85c1efc81d77d25e64068d08c13c74057b66e68
bowook <mbosx316@naver.com>
2024-11-30 18:38:55 +0900

    feat : Parser 기능 추가

--- src/main/java/bridge/utils/Parser.java
+++ src/main/java/bridge/utils/Parser.java
@@ -1,2 +1,14 @@
-package bridge.utils;public class Parser {
+package bridge.utils;
+
+import bridge.exception.CustomException;
+import bridge.exception.ErrorMessage;
+
+public class Parser {
+    public static int toNumber(String input) {
+        try {
+            return Integer.parseInt(input);
+        } catch (NumberFormatException numberFormatException) {
+            throw CustomException.from(ErrorMessage.INPUT_FORMAT_ERROR);
+        }
+    }
 }
