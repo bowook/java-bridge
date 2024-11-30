6cf72767676344949b67f92e30a2ec58718f2712
bowook <mbosx316@naver.com>
2024-11-30 18:37:23 +0900

    feat : LengthCommand 기능 추가

--- src/main/java/bridge/domain/constant/LengthCommand.java
+++ src/main/java/bridge/domain/constant/LengthCommand.java
@@ -1,2 +1,25 @@
-package bridge.domain.constant;public enum LengthCommand {
+package bridge.domain.constant;
+
+import bridge.exception.CustomException;
+import bridge.exception.ErrorMessage;
+
+public enum LengthCommand {
+    MIN(3),
+    MAX(20);
+    private final int length;
+
+    LengthCommand(final int length) {
+        this.length = length;
+    }
+
+    public static int from(int input) {
+        if (input < MIN.length || input > MAX.length) {
+            throw CustomException.from(ErrorMessage.INPUT_ERROR);
+        }
+        return input;
+    }
+
+    public int getLength() {
+        return length;
+    }
 }
