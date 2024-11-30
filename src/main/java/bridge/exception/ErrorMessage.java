27cc5f578445f88f9c4aea45e974e44c968ef01d
bowook <mbosx316@naver.com>
2024-11-30 18:38:14 +0900

    feat : ErrorMessage 기능 추가

--- src/main/java/bridge/exception/ErrorMessage.java
+++ src/main/java/bridge/exception/ErrorMessage.java
@@ -1,2 +1,18 @@
-package bridge.exception;public enum ErrorMessage {
+package bridge.exception;
+
+public enum ErrorMessage {
+    INPUT_ERROR("3 이상 20 이하의 숫자를 입력하셔야 합니다."),
+    INPUT_FORMAT_ERROR("숫자를 입력하셔야 합니다."),
+    GAME_COMMAND_ERROR("R(재시작)과 Q(종료) 중 하나의 문자를 입력하셔야 합니다."),
+    MOVE_COMMAND_ERROR("U(위 칸)와 D(아래 칸) 중 하나의 문자를 입력하셔야 합니다.");
+
+    private final String message;
+
+    ErrorMessage(String message) {
+        this.message = message;
+    }
+
+    public String getMessage() {
+        return this.message;
+    }
 }
