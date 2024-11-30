0e34b5ed35647f09df0ffe868b66ea56d86bcf29
bowook <mbosx316@naver.com>
2024-11-30 18:36:29 +0900

    feat : BridgeController 기능 추가

--- src/main/java/bridge/controller/BridgeController.java
+++ src/main/java/bridge/controller/BridgeController.java
@@ -1,2 +1,32 @@
-package bridge.controller;public class BridgeController {
+package bridge.controller;
+
+import bridge.exception.CustomException;
+import bridge.view.InputView;
+import bridge.view.OutputView;
+
+public class BridgeController {
+    private final InputView inputView;
+    private final OutputView outputView;
+    private final BridgeGame bridgeGame;
+
+    public BridgeController(final InputView inputView, final OutputView outputView, final BridgeGame bridgeGame) {
+        this.inputView = inputView;
+        this.outputView = outputView;
+        this.bridgeGame = bridgeGame;
+    }
+
+    public void start() {
+        outputView.printStart();
+        int bridgeLength = readBridgeLength();
+    }
+
+    private int readBridgeLength() {
+        while (true) {
+            try {
+                return inputView.readBridgeSize();
+            } catch (CustomException customException) {
+                outputView.printErrorMessage(customException.getMessage());
+            }
+        }
+    }
 }
