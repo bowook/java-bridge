9b184c3706401e1d02fb88d81367ebe2b6899dd4
bowook <mbosx316@naver.com>
2024-11-30 18:39:10 +0900

    feat : InputView 기능 추가

--- src/main/java/bridge/view/InputView.java
+++ src/main/java/bridge/view/InputView.java
@@ -1,15 +1,22 @@
-package bridge;
+package bridge.view;
+
+import bridge.domain.constant.LengthCommand;
+import bridge.utils.Parser;
+import java.util.Scanner;
 
 /**
  * 사용자로부터 입력을 받는 역할을 한다.
  */
 public class InputView {
+    private final static Scanner scanner = new Scanner(System.in);
+    private final static String INPUT_BRIDGE_LENGTH = "다리의 길이를 입력해주세요.";
 
     /**
      * 다리의 길이를 입력받는다.
      */
     public int readBridgeSize() {
-        return 0;
+        System.out.println(INPUT_BRIDGE_LENGTH);
+        return LengthCommand.from(Parser.toNumber(readConsole()));
     }
 
     /**
@@ -24,5 +31,9 @@ public class InputView {
      */
     public String readGameCommand() {
         return null;
+    }
+
+    private String readConsole() {
+        return scanner.nextLine();
     }
 }
