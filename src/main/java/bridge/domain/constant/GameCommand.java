98456550f9ad603d662c7762adf2c0b44e073784
bowook <mbosx316@naver.com>
2024-11-30 18:37:06 +0900

    feat : GameCommand 기능 추가

--- src/main/java/bridge/domain/constant/GameCommand.java
+++ src/main/java/bridge/domain/constant/GameCommand.java
@@ -1,2 +1,27 @@
-package bridge.domain.constant;public class GameCommand {
+package bridge.domain.constant;
+
+import bridge.exception.CustomException;
+import bridge.exception.ErrorMessage;
+import java.util.Arrays;
+
+public enum GameCommand {
+    RETRY("R"),
+    QUIT("Q");
+
+    private final String gameCommand;
+
+    GameCommand(final String gameCommand) {
+        this.gameCommand = gameCommand;
+    }
+
+    public static GameCommand from(String input) {
+        return Arrays.stream(GameCommand.values())
+                .filter(command -> command.gameCommand.equals(input))
+                .findAny()
+                .orElseThrow(() -> CustomException.from(ErrorMessage.GAME_COMMAND_ERROR));
+    }
+
+    public String getGameCommand() {
+        return gameCommand;
+    }
 }
